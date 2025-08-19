52a16005dd4f6410d75bf2bffdb9a831f6291f9d
berryicebox <feelmypasta@gmail.com>
2025-08-19 23:20:23 +0900

    react-auth: action 함수로 계정생성 및 로그인 처리

--- react-auth/01-starting-project/frontend/src/pages/Authentication.js
+++ react-auth/01-starting-project/frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { redirect } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -5,3 +6,38 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+export async function action({request}) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+
+  if (mode !== 'login' && mode !== 'signup') {
+    throw JSON.stringify({ message: 'Unsupporeted mode.' }, { status: 400 });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  }
+
+  const response = await fetch('http://localhost:8080/' + mode, {
+    method: 'POST',
+    body: JSON.stringify(authData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    const errorJson = await response.json();
+    console.error('Signup Error:', errorJson);
+    return response;
+  }
+
+  if (!response.ok) {
+    throw new Response({ message: 'Could not authenticate user.' }, { status: 500 });
+  }
+
+  return redirect('/');
+}
