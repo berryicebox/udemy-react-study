f3a0bd87ddd75d36c4f14abeb79c1c538fda9456
berryicebox <feelmypasta@gmail.com>
2025-08-25 23:07:53 +0900

    react-auth: 로그아웃
    1. 로그아웃 - action과 Form 이용
    2. 토큰 전역 관리 - loader 함수 이용

--- react-auth/01-starting-project/frontend/src/components/MainNavigation.js
+++ react-auth/01-starting-project/frontend/src/components/MainNavigation.js
@@ -1,9 +1,11 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Form, useRouteLoaderData } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 import NewsletterSignup from './NewsletterSignup';
 
 function MainNavigation() {
+  const token = useRouteLoaderData('root');
+
   return (
     <header className={classes.header}>
       <nav>
@@ -40,14 +42,23 @@ function MainNavigation() {
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/auth?mode=login"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Authentication
-            </NavLink>
+            {!token && (
+              <NavLink
+                to="/auth?mode=login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Authentication
+              </NavLink>
+            )}
+          </li>
+          <li>
+            {token && (
+              <Form method="post" action="/logout">
+                <button>Logout</button>
+              </Form>
+            )}
           </li>
         </ul>
       </nav>
