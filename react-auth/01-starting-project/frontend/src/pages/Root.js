d49e11af924b95369cd597203526ffd71e34bb96
berryicebox <feelmypasta@gmail.com>
2025-08-26 23:03:35 +0900

    react-auth: 라우트 보호, 자동 로그아웃
    1. 라우트 보호: loader 이용
    2. 자동 로그아웃: useEffect, setTimeout, 토큰 유효시간 관리

--- react-auth/01-starting-project/frontend/src/pages/Root.js
+++ react-auth/01-starting-project/frontend/src/pages/Root.js
@@ -1,9 +1,30 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, { method: 'post', action: '/logout' });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log('Token duration (ms):', tokenDuration);
+
+    setTimeout(() => {
+      submit(null, { method: 'post', action: '/logout' });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
