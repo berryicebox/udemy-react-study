478f85256866ea17f6d8e52bf2cbc7df5d3dba48
berryicebox <feelmypasta@gmail.com>
2025-11-05 23:39:34 +0900

    react-ts: 기본형, 유니언, void, 타입 추론, 타입 별칭
    - 타입 별칭을 통해 타입 선언 코드 중복을 줄일 수 있다
    - 타입 추론 기능이 작동하므로 일반적인 경우엔 굳이 타입을 지정하지 않는걸 권장한다

---
+++ react-ts/01-starting-project/basics.ts
@@ -0,0 +1,32 @@
+// 기본형: nuber, string, boolean
+let age: number = 12;
+let userName: string = "Max";
+let isInstructor: boolean = true;
+
+// 배열, 객체
+let hobbies: string[] = ["Sports", "Cooking"];
+let person: { name: string; age: number } = {
+  name: "Max",
+  age: 32,
+};
+// person = { isEmployee: true }; 오류 발생
+
+// 타입 추론
+let course = "React - The Complete Guide";
+// course = 12341; 오류 발생
+
+// 유니언 타입
+let unionType: string | number = "React";
+unionType = 12341;
+
+// 타입 별칭
+type Person = { name: string; age: number };
+let anotherPerson: Person = {
+  name: "Anna",
+  age: 25,
+};
+
+// void
+function printOutput(value: string | number): void {
+  console.log(value);
+}
